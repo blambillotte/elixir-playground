7450d94df46c770850a32734e1070ccc3ea343e0
Ben Lambillotte <ben.lambillotte@gmail.com>
2019-01-06 17:13:54 -0800

    More recursion tests

--- scratchpad/recursion.exs
+++ scratchpad/recursion.exs
@@ -1,5 +1,15 @@
 defmodule ListUtils do
 
+  def max([ current_maximum | [ head | tail]]) when current_maximum < head do
+    max [head | tail]
+  end
+
+  def max([ current_maximum | [ head | tail]]) when current_maximum >= head do
+    max [current_maximum | tail]
+  end
+
+  def max([current_maximum]), do: current_maximum # found max
+
   def map([], _fn), do: []
   def map([head | tail], fun) do
     [ fun.(head) | map(tail, fun) ]
@@ -16,4 +26,7 @@ ListUtils.mult([1,2,3])
   |> IO.puts
 
 ListUtils.map([2,3,4], fn(a) -> (a * 3) end)
+  |> IO.inspect
+
+ListUtils.max([2,3,50, 30, 15, 20])
   |> IO.inspect
